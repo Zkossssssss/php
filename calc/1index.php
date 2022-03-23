d8d2ff7201ff8696773f4a9dd0fe60ca5ba3bd42
Konstantin <k_bobrovskiy03@mail.ru>
2022-03-23 13:22:13 +0300

    calculator

---
+++ calc/1index.php
@@ -0,0 +1,13 @@
+<?php
+header('Context-Type: text/html; charset=utf-8');
+$a = $_POST['ch1'];
+$b = $_POST['ch2'];
+$d = $_POST['deist'];
+$c = 0;
+if ($d='+') {$c = $a+$b;}
+else if ($d='-') {$c = $a-$b;}
+else if ($d='*') {$c = $a*$b;}
+else if ($d='/') {$c = $a/$b;}
+else {'Неверная операция';}
+echo $a, ' ', $d, ' ', $b, '=', $c;
+?>
