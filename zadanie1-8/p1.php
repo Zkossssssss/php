f41621e786e3d89d508fd97ccf599e4973800379
Konstantin <k_bobrovskiy03@mail.ru>
2022-03-29 19:19:25 +0300

    zadanie 2

--- zadanie1-8/p1.php
+++ zadanie1-8/p1.php
@@ -1,6 +1,9 @@
+
 <?php
 	header('Context-Type: text/html; charset=utf-8');	
-	$a = $_POST['vvodcel']; 
-	$r = $a * (9/5) + 32; 
-	echo $r;
-?> 
+	$a = $_POST['vvod']; 
+	$fact = 1; 
+	for ($i = 1; $i <= $a; $i++)
+	{$fact *= $i;}
+	echo $fact;
+?>
