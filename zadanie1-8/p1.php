5422be09b718cf0db3b44d0c89a06d53bd7166ab
Konstantin <k_bobrovskiy03@mail.ru>
2022-03-29 19:55:05 +0300

    zadanie 6

--- zadanie1-8/p1.php
+++ zadanie1-8/p1.php
@@ -1,37 +1,18 @@
 
 <?php
-function discriminant($a, $b, $c) {
-$d = pow($b, 2) - 4 * $a * $c;
-return $d;
-}
-
-function root_1($a, $b, $d) {
-$x_1 = (-$b + sqrt($d))/(2 * $a);
-return $x_1;
-}
-
-function root_2($a, $b, $d) {
-$x_1 = (-$b - sqrt($d))/(2 * $a);
-return $x_1;
-}
-
 if(!empty($_REQUEST)) {
-if(count($_REQUEST) == 3 and $_REQUEST['a'] != 0 and $_REQUEST['b'] != 0 and $_REQUEST['c'] !=
-0) {
-$discriminant = discriminant($_REQUEST['a'], $_REQUEST['b'], $_REQUEST['c']);
-if($discriminant > 0) {
-echo "Корень уравнения 1: ".root_1($_REQUEST['a'], $_REQUEST['b'], $discriminant).'<br>';
-echo "Корень уравнения 2: ".root_2($_REQUEST['a'], $_REQUEST['b'], $discriminant);
+if(
+count($_REQUEST) == 3 and
+(isset($_REQUEST[1]) and strlen($_REQUEST[1]) > 0) and
+(isset($_REQUEST[2]) and strlen($_REQUEST[2]) > 0) and
+(isset($_REQUEST[3]) and strlen($_REQUEST[3]) > 0)
+) {
+sort($_REQUEST);
+if(pow($_REQUEST[2], 2) == (pow($_REQUEST[0], 2)) + pow($_REQUEST[1], 2)) {
+echo implode(', ', $_REQUEST)." Числа - тройка Пифагора";
 }
-
-if($discriminant == 0) {
-echo "Корень уравнения: ".root_1($_REQUEST['a'], $_REQUEST['b'], $discriminant);
+else echo "Числа: ".implode(', ', $_REQUEST)." не тройка Пифагора";
 }
-
-if($discriminant < 0) {
-echo "Уравнение не имеет корней.";
-}}
-
-else echo "Поле не может быть пустым или равным нулю!";
+else echo "Поля пусты. Введите числа";
 }
-?> 
+?>
