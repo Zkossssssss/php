28536dedf2e23ee610fce2cfdf00659ea83bbd5c
Konstantin <k_bobrovskiy03@mail.ru>
2022-03-29 19:42:03 +0300

    zadanie 4

--- zadanie1-8/p1.php
+++ zadanie1-8/p1.php
@@ -1,9 +1,9 @@
-
 <?php
-    $a = $_REQUEST['vvod'];
+    $a = $_REQUEST['vvod1'];
+    $b = $_REQUEST['vvod2'];
     for($i = 1; $i <= $a; $i++) {
-        if($a % $i == 0) {
+        if(($a % $i == 0) and ($b % $i == 0)) {
             $arr[] = $i;
         }}
-        echo "Делители числа ".$a.": ".implode(', ', $arr);
+        echo " Общие делители ".$a." и ".$b.": ".implode(', ', $arr);
 ?>
