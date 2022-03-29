9921ec01a181ffe39930788706a03bab4de27164
Konstantin <k_bobrovskiy03@mail.ru>
2022-03-29 20:29:49 +0300

    profile

---
+++ profile/profile.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Info</title>
+	</head> 
+<body>
+    <form action='' method='post'>
+		<input type='text' name='name' placeholder='Имя'><br>
+		<input type='text' name='surname' placeholder='Фамилия'><br>
+		<input type='submit'>
+	</form>
+</body>
+</html> 
