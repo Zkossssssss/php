685e6f1e50a338d4da7df10c46f3b6f5fdb13fb8
Konstantin <k_bobrovskiy03@mail.ru>
2022-03-29 19:46:51 +0300

    zadanie 5

--- zadanie1-8/p1.php
+++ zadanie1-8/p1.php
@@ -1,9 +1,37 @@
+
 <?php
-    $a = $_REQUEST['vvod1'];
-    $b = $_REQUEST['vvod2'];
-    for($i = 1; $i <= $a; $i++) {
-        if(($a % $i == 0) and ($b % $i == 0)) {
-            $arr[] = $i;
-        }}
-        echo " Общие делители ".$a." и ".$b.": ".implode(', ', $arr);
-?>
+function discriminant($a, $b, $c) {
+$d = pow($b, 2) - 4 * $a * $c;
+return $d;
+}
+
+function root_1($a, $b, $d) {
+$x_1 = (-$b + sqrt($d))/(2 * $a);
+return $x_1;
+}
+
+function root_2($a, $b, $d) {
+$x_1 = (-$b - sqrt($d))/(2 * $a);
+return $x_1;
+}
+
+if(!empty($_REQUEST)) {
+if(count($_REQUEST) == 3 and $_REQUEST['a'] != 0 and $_REQUEST['b'] != 0 and $_REQUEST['c'] !=
+0) {
+$discriminant = discriminant($_REQUEST['a'], $_REQUEST['b'], $_REQUEST['c']);
+if($discriminant > 0) {
+echo "Корень уравнения 1: ".root_1($_REQUEST['a'], $_REQUEST['b'], $discriminant).'<br>';
+echo "Корень уравнения 2: ".root_2($_REQUEST['a'], $_REQUEST['b'], $discriminant);
+}
+
+if($discriminant == 0) {
+echo "Корень уравнения: ".root_1($_REQUEST['a'], $_REQUEST['b'], $discriminant);
+}
+
+if($discriminant < 0) {
+echo "Уравнение не имеет корней.";
+}}
+
+else echo "Поле не может быть пустым или равным нулю!";
+}
+?> 
