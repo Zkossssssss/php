3ba6a8307035805d897d731c7949e9eb1c59f4f5
Konstantin <k_bobrovskiy03@mail.ru>
2022-03-30 10:07:06 +0300

    zadanie 9

--- zadanie1-8/p1.php
+++ zadanie1-8/p1.php
@@ -1,9 +1,22 @@
-
-<?php
-if (!empty($_REQUEST['text'])) {
-    $str = $_REQUEST['text'];
-    $stroka = mb_strlen($str, 'utf-8');
-    $word = count(explode(' ', $str));
-    $space = $word - 1;
-    echo 'Здесь '.$space. ' пробелов, '.$word.' слов, '.$stroka.' символов.';
+ <?php
+if(isset($_REQUEST['submit']) && !empty($_REQUEST['text'])){
+$text = $_REQUEST['text'];
+$length = mb_strlen($text,'UTF-8');
+$arr = [];
+for($i=0;$i < $length;$i++){
+$char = mb_substr($text,$i,1,'UTF-8');
+if(!isset($arr[$char])){
+$arr[$char] = 1;
+} else{
+$arr[$char]++;
 }
+}
+foreach($arr as $key=>$elem){
+$percent = round($elem * 100 / $length, 0);
+echo "Процентное содержание '$key'".' в тексте - '.$percent.'%<br>';
+}
+}
+if(isset($_REQUEST['submit']) && empty($_REQUEST['text'])){
+echo 'Введите текст в   форму';
+}
+?>
