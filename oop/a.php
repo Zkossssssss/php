bb07efb2ab929fb12d879e3c22682c1ee7157e8e
Konstantin <k_bobrovskiy03@mail.ru>
2022-03-30 11:44:50 +0300

    save

--- oop/a.php
+++ oop/a.php
@@ -1,16 +1,25 @@
 <?php 
+session_start();
 header('Content-Type: text/html; charset=utf-8');
 class User {
-	public $name;
-	public $login;
+	private $name;
+	private $login;
 	private $pass;
+	
+	public function set($n, $l, $p) {
+	$this->name = $n;	
+	$this->login = $l;
+	$this->pass = $p;
+	} 
+	public function SaveToSession() {
+		$_SESSION[user_name] = $this->name;
+		$_SESSION[user_login] = $this->login;
+		$_SESSION[user_pass] = $this->pass;
+	}
+	
 	public function show() {
 		echo '<h3> Имя: '.$this->name.'</h3>';
 		echo '<h3> Логин: '.$this->login.'</h3>';
 	}
 }
-$user = new User();
-$user->name = 'Konstantin';
-$user->login = 'admin';
-$user->show();
 ?>
