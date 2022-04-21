4c1ef0ddde2e9beb279ed6f7e3a67f39fc56d80b
Konstantin <k_bobrovskiy03@mail.ru>
2022-04-21 20:07:06 +0300

    glava_94_2-3

--- framework/project/config/routes.php
+++ framework/project/config/routes.php
@@ -8,5 +8,7 @@
 		new Route('/poka/', 'test', 'act2'),
 		new Route('/kak/', 'test', 'act3'),
 		new Route('/nums/:n1/:n2/:n3/', 'num', 'sum'),
+		new Route('/user/:id/', 'user', 'show'),
+		new Route('/user/:id/:key/', 'user', 'info'),
 	];
-?> 
+?>
